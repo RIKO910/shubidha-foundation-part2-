6b00f8f9c0b88e2a0ed3080fb359614686367463
RIKO910 <tarikulislamriko910@gmail.com>
2022-11-25 23:03:05 +0600

    ok

--- part2.js
+++ part2.js
@@ -3,6 +3,15 @@ const btn =document.getElementById('btn');
 btn.addEventListener('click', ()=>{
     console.log('ok')
     const form = document.getElementById('form');
+    if (form.style.visibility === 'hidden') {
+        form.style.visibility = 'visible';
+      } else {
+        form.style.visibility = 'hidden';
+      }
+})
+btn.addEventListener('click', ()=>{
+    console.log('ok')
+    const form = document.getElementById('form');
     if(form.style.display == 'none'){
         form.style.display = 'block';
     }
